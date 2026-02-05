73de0b3d8f8c2322afe5086b03e6a89edbb8adb0
JayHwang <hjik7297@gmail.com>
2026-02-05 02:00:45 -0600

    feat: added app.use

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express, { Express } from "express";
 
+// import Routes modules
+import healthRoutes from "././api/v1/routes/healthRoutes"
+import userRoutes from "././api/v1/routes/userRoutes"
+
 // Initialize Express application
 const app: Express = express();
 
@@ -7,5 +11,12 @@ const app: Express = express();
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
+
+app.use(express.json());
+
+// router handler
+// router defined in health routes, prefixed with /api/v1
+app.use("/api/v1", healthRoutes);
+app.use("/api/v1", userRoutes);
 
 export default app;
