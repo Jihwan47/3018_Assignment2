e5e538d541238f6526434e018f5081b83b7b232b
JayHwang <hjik7297@gmail.com>
2026-02-05 02:23:21 -0600

    feat: modification in structure

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
 import express, { Express } from "express";
 
 // import Routes modules
-import healthRoutes from "././api/v1/routes/healthRoutes"
-import userRoutes from "././api/v1/routes/userRoutes"
+import healthRoutes from "././api/v1/routes/healthRoutes";
+import userRoutes from "././api/v1/routes/userRoutes";
 
 // Initialize Express application
 const app: Express = express();
+app.use(express.json());
 
 // Define a route
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
-
-app.use(express.json());
 
 // router handler
 // router defined in health routes, prefixed with /api/v1
