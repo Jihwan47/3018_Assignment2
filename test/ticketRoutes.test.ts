c9a71625ab58a059f940aa4de95860db42a932ac
JayHwang <hjik7297@gmail.com>
2026-02-07 03:14:57 -0600

    feat: modification on comments

--- test/ticketRoutes.test.ts
+++ test/ticketRoutes.test.ts
@@ -4,6 +4,8 @@ import * as healthController from "../src/api/v1/controllers/healthController";
 import app from "../src/app";
 import { Priority, Status, Ticket, TicketUrgency, calculateDay } from "../src/api/v1/services/ticketService";
 
+// set this controller function to return 200 default, so that i can check whether 
+// the router correctly calls the controller without testing the actual logic or using API
 jest.mock("../src/api/v1/controllers/ticketController", () => ({
     healthCheck: jest.fn((req,res) => res.status(200).send()),
     createTicket: jest.fn((req, res) => res.status(200).send()),
@@ -13,44 +15,48 @@ jest.mock("../src/api/v1/controllers/ticketController", () => ({
     deleteTicket: jest.fn((req, res) => res.status(200).send()),
     getUrgentTicketById: jest.fn((req, res) => res.status(200).send())
 }));
-
 jest.mock("../src/api/v1/controllers/healthController", () => ({
     healthCheck: jest.fn((req,res) => res.status(200).send())
 }));
 
+// Main test suite for routes using Mock
 describe("Mock healthRoutes/ticketRoutes testing", () => {
 
+    // Reset mock before each time
     beforeEach(() => {
         jest.clearAllMocks();
     });
 
+    // test health endpoint
 	describe("GET /api/v1/health", () => {
 		it("should call healthCheck function from healthController", async () => {
             // Arrange
 
-            // Act
+            // Act: send GET request to health endpoint
 			await request(app).get("/api/v1/health");
 
-            // Assert
+            // Assert: ensure that controller function was called
 			expect(healthController.healthCheck).toHaveBeenCalled();
 		});
 	});
 
+    // test retrieve all tickets
 	describe("Get /api/v1/tickets", () => {
 		it("should call getAllTickets function from ticketController", async () => {
 			// Arrange
 
-            // Act
+            // Act: send GET request to tickets endpoint
 			await request(app).get("/api/v1/tickets");
 
-            // Assert
+            // Assert: ensure that controller function was called
 			expect(ticketController.getAllTickets).toHaveBeenCalled();
 		});
 	});
 
+    // test retrieve ticket by id
     describe("GET /api/v1/tickets/:id", () => {
 		it("should call getTicketById function from ticketController", async () => {
-			// Arrange
+			// Arrange: create a mock ticket item
             const mockItem: Ticket = {
                 id: 2, 
                 title: "Profile picture upload slow", 
@@ -60,64 +66,66 @@ describe("Mock healthRoutes/ticketRoutes testing", () => {
                 createdAt: calculateDay(2)
             }
 
-            // Act
+            // Act: send Get reqeust to the ticket id endpoint
             await request(app).get("/api/v1/tickets/2").send(mockItem);
 
-            // Assert
+            // Assert: ensure that controller function was called
 			expect(ticketController.getTicketById).toHaveBeenCalled();
 		});
 	});
 
-
+    // test create ticket
     describe("POST /api/v1/tickets", () => {
 		it("should call createTicket from ticketController", async () => {
-            // Arrange
+            // Arrange: create a mock ticket item
             const mockItem = {
                 "title": "Testing on createTicket function with Mock item",
                 "description": "Mocking fake item to test codes' behavior",
                 "priority": Priority.Medium
             };
 
-            // Act
+            // Act: send POST reqeust to the ticket endpoint
             await request(app).post("/api/v1/tickets").send(mockItem);
 
-            // Assert
+            // Assert: ensure that controller function was called
 			expect(ticketController.createTicket).toHaveBeenCalled();
 		});
 	});
-
+    
+    // test update ticket
     describe("PUT /api/v1/tickets/:id", () => {
 		it("should call updateTicket from ticketController", async () => {
-            // Arrange
+            // Arrange: create a mock ticket item
             const mockItem = {
                 "id": 1,
                 "priority": Priority.Critical,
                 "status": Status.Resolved
             };
             
-            // Act
+            // Act: send PUT reqeust to the ticket id endpoint
             await request(app).put("/api/v1/tickets/1").send(mockItem);
 
-            // Assert
+            // Assert: ensure that controller function was called
 			expect(ticketController.updateTicket).toHaveBeenCalled();
 		});
 	});
 
+    // test delete ticket
     describe("DELETE /api/v1/tickets/:id", () => {
 		it("should call updateTicket from ticketController", async () => {
             // Arrange
             
-            // Act
+            // Act: send DELETE request to the ticket id endpoint
             await request(app).delete("/api/v1/tickets/5");
 
-            // Assert
+            // Assert: ensure that controller function was called
 			expect(ticketController.deleteTicket).toHaveBeenCalled();
 		});
 	});
-
+    // test get urgentTicket 
     describe("GET /api/v1/tickets/:id/urgency", () => {
 		it("should call getUrgentTicketById function from ticketController", async () => {
-			// Arrange
+			// Arrange: create a mock ticket item
             const mockItem: TicketUrgency = {
                 id: 4, 
                 title: "Password reset email delayed", 
@@ -129,10 +137,10 @@ describe("Mock healthRoutes/ticketRoutes testing", () => {
                 urgencyLevel: "High urgency, Prioritize resolution."
             }
 
-            // Act
+            // Act: send GET reqeust to the ticket id urgency endppoint
             await request(app).get("/api/v1/tickets/4/urgency").send(mockItem);
 
-            // Assert
+            // Assert: ensure that controller function was called
 			expect(ticketController.getUrgentTicketById).toHaveBeenCalled();
 		});
 	});
